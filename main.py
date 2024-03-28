67ab68ac9f3107ccd5804e68d7d305e546e83bf4
whoami <psychopo@icloud.com>
2024-03-28 10:15:37 +0700

    setup ui

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from os import getcwd
+from PyQt6 import QtWidgets
+from ui.main_window import Ui_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+	'''MainWindow class'''
+	def __init__(self):
+		super().__init__()
+
+		self.setupUi(self)
+
+
 def main():
-	pass
+	app = QtWidgets.QApplication([])
+	window = MainWindow()
+	window.show()
+	app.exec()
 
 if __name__ == "__main__":
 	main()
